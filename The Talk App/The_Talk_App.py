d4e847a5b59d2fa332b4602f62b85f59dafff057
Sebik <Sebik@DELL>
2021-12-11 20:04:04 +0100

    Add project files.

---
+++ The Talk App/The_Talk_App.py
@@ -0,0 +1,17 @@
+
+#A simple program that uh
+#can have a smol smol veri smol
+#conversation
+
+user_name == input()
+is_gud_or_bad == input()
+
+print("What is name?")
+user_name
+print("Hello", +user_name, "how is your day going?")
+is_gud_or_bad
+if is_gud_or_bad == "Good, what about you?":
+    print("I'm good as usual.")
+elif is_gud_or_bad == "Bad.":
+    print("Oh, I'm sorry to hear that.");
+
